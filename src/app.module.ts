3fcc45b600d4b4d3a2f66d5c8492404000d5b31e
AAMCortez <aamcortez@gmail.com>
2023-09-13 20:45:39 +0100

    auth done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { GameModule } from './game/game.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, GameModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    GameModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
