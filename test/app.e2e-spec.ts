de82a728726ea982c06c411bd3e962977b3d7b7f
AAMCortez <aamcortez@gmail.com>
2023-09-15 18:18:47 +0100

    more tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,6 +4,7 @@ import { AppModule } from '../src/app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { PrismaService } from '../src/prisma/prisma.service';
 import { AuthDto } from 'src/auth/dto';
+import { EditUserDto } from 'src/user/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -87,7 +88,7 @@ describe('App e2e', () => {
           .post('/auth/login')
           .withBody(dto)
           .expectStatus(200)
-          .stores('userAt', 'acess_token');
+          .stores('userAt', 'access_token');
       });
     });
   });
@@ -101,16 +102,31 @@ describe('App e2e', () => {
           .expectStatus(200);
       });
     });
-    describe('Edit user', () => {});
+    describe('Edit user', () => {
+      it('should edit user', () => {
+        const dto: EditUserDto = {
+          firstName: 'Antonio',
+          email: 'ac@ac.com',
+        };
+        return pactum
+          .spec()
+          .patch('/user')
+          .withHeaders({ Authorization: 'Bearer $S{userAt}' })
+          .withBody(dto)
+          .expectStatus(200)
+          .expectBodyContains(dto.firstName)
+          .expectBodyContains(dto.email);
+      });
+    });
   });
   describe('Game', () => {
     describe('Create game', () => {});
     describe('Get game', () => {});
-    describe('Edit game', () => {});
-    describe('Delete game', () => {});
+    describe('Edit game by id', () => {});
+    describe('Delete game by id', () => {});
   });
   describe('Bet', () => {
-    describe('Make bet', () => {});
+    describe('Make bet by gameId', () => {});
     describe('Get bet', () => {});
   });
 });
